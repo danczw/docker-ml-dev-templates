5bd6996761870ec61020747ec0e44617d63000ec
danczw <dev.dc@pm.me>
2022-07-24 23:12:07 +0200

    add pytorch huggingface image

---
+++ 04_pytorch_huggingface_image/src/main.py
@@ -0,0 +1,14 @@
+# imports
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+# from torch import ...
+# from torchvision import ...
+# from datasets import ...
+# from transformers import ...
+
+def main():
+    print('hello world')
+
+if __name__ == '__main__':
+    main()
