86f94ddea28a409bddd06bf56c8db70ee8a0b7cf
danczw <dev.dc@pm.me>
2022-07-24 22:25:16 +0200

    add tensorflow huggingface image

---
+++ 02_tensorflow_huggingface_image/src/main.py
@@ -0,0 +1,13 @@
+# imports
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+# from tensorflow import ...
+# from datasets import ...
+# from transformers import ...
+
+def main():
+    print('hello world')
+
+if __name__ == '__main__':
+    main()
